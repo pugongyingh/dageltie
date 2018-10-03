39dba5d0e02a162ffea01e8edcb4d8f08779866e
Richard Flosi <richard.flosi@gmail.com>
2018-10-02 23:27:51 -0700

    Moving console logs into foo function

--- functions/foo.js
+++ functions/foo.js
@@ -1,18 +1,18 @@
-try {
-  const TOML = require("@iarna/toml");
-  console.log("foo: required toml");
-  console.log("TOML", typeof TOML);
-} catch (error) {
-  console.log(`foo: error requiring toml; ${error}`);
-}
-
-const statusCode = 200;
-const headers = {
-  "Access-Control-Allow-Origin" : "*",
-  "Access-Control-Allow-Headers": "Content-Type"
-};
-
 exports.handler = function(event, context, callback) {
+  try {
+    const TOML = require("@iarna/toml");
+    console.log("foo: required toml");
+    console.log("TOML", typeof TOML);
+  } catch (error) {
+    console.log(`foo: error requiring toml; ${error}`);
+  }
+  
+  const statusCode = 200;
+  const headers = {
+    "Access-Control-Allow-Origin" : "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+  };
+  
   callback(null, {
     statusCode,
     headers,
