f113a60663e7daf774811804fe85a53daa932081
Richard Flosi <richard.flosi@gmail.com>
2018-10-02 23:43:54 -0700

    more logs in foo

--- functions/foo.js
+++ functions/foo.js
@@ -6,8 +6,10 @@ try {
 }
 
 exports.handler = function(event, context, callback) {  
-  console.log("TOML", typeof TOML);
-  console.log("TOML", Object.keys(TOML));
+  console.log("TOML type", typeof TOML);
+  console.log("TOML keys", Object.keys(TOML));
+  console.log("TOML.code", TOML.code);
+  console.log("TOML.parse", TOML.parse);
   const statusCode = 200;
   const headers = {
     "Access-Control-Allow-Origin" : "*",
